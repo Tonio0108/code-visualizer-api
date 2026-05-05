9547561fe8c9d2464dc1117774d9053da433f184
Antonio Andriatsiaforitrarivo <tonioandria2@gmail.com>
2026-05-05 21:37:09 +0300

    fix main.ts error

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,36 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 
+let cachedServer: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  // Security
-  app.use(helmet());
-  app.enableCors();
-  
-  // Global configurations
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    
+    // Security
+    app.use(helmet());
+    app.enableCors();
+    
+    // Global configurations
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
 
-  await app.init();
-  return app;
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer;
 }
 
-// Pour le déploiement local
-if (process.env.NODE_ENV !== 'production') {
-  bootstrap().then(app => app.listen(process.env.PORT ?? 3000));
-}
-
-// Pour Vercel (exportation du handler)
-export const handler = async (req: any, res: any) => {
-  const app = await bootstrap();
-  const instance = app.getHttpAdapter().getInstance();
-  instance(req, res);
+// Export the handler for Vercel
+export default async (req: any, res: any) => {
+  const server = await bootstrap();
+  return server(req, res);
 };
 
-export default handler;
